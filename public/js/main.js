038fc527c5a73a127bbb94ce05b2cebf7bf7a9c5
Ryan Kuck <ryankuck@gmail.com>
2019-08-16 19:08:49 -0400

    scroll down on click working

--- public/js/main.js
+++ public/js/main.js
@@ -4,6 +4,45 @@
   once: true
  });
 
+
+// Select all links with hashes
+$('a[href*="#"]')
+  // Remove links that don't actually link to anything
+  .not('[href="#"]')
+  .not('[href="#0"]')
+  .click(function(event) {
+    // On-page links
+    if (
+      location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') 
+      && 
+      location.hostname == this.hostname
+    ) {
+      // Figure out element to scroll to
+      var target = $(this.hash);
+      target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+      // Does a scroll target exist?
+      if (target.length) {
+        // Only prevent default if animation is actually gonna happen
+        event.preventDefault();
+        $('html, body').animate({
+          scrollTop: target.offset().top
+        }, 1000, function() {
+          // Callback after animation
+          // Must change focus!
+          var $target = $(target);
+          $target.focus();
+          if ($target.is(":focus")) { // Checking if the target was focused
+            return false;
+          } else {
+            $target.attr('tabindex','-1'); // Adding tabindex for elements not focusable
+            $target.focus(); // Set focus again
+          };
+        });
+      }
+    }
+  });
+  
+
 (function($) {
 
 	'use strict';
@@ -19,7 +58,6 @@
     }, 1);
   };
   loader();
-
 	
 	$('nav .dropdown').hover(function(){
 		var $this = $(this);
@@ -33,8 +71,9 @@
 			$this.find('.dropdown-menu').removeClass('show');
 	});
 
+  // $('col-md-8 text-center col-sm-12 ').scrollTo(document.getElementById('div1'), 1000);
 
-	$('#dropdown04').on('show.bs.dropdown', function () {
+  $('#dropdown04').on('show.bs.dropdown', function () {
 	  console.log('show');
 	});
 
