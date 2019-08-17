e9844cbcedfa356c11705242da73bb6f7ecd541a
Ryan Kuck <ryankuck@gmail.com>
2019-08-17 18:19:02 -0400

    Updates

---
+++ precache-manifest.18520529f8958e7aca2ace89946ebf42.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8686bb2a1ffb9861653ba74f124ad68b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "59c0528cff7aca95971e",
+    "url": "/static/css/main.2e42db67.chunk.css"
+  },
+  {
+    "revision": "2571f7b495a6873c1811",
+    "url": "/static/js/2.5362d1b6.chunk.js"
+  },
+  {
+    "revision": "59c0528cff7aca95971e",
+    "url": "/static/js/main.22b1f376.chunk.js"
+  },
+  {
+    "revision": "ae192a9a122a633acbd6",
+    "url": "/static/js/runtime~main.a65176cb.js"
+  }
+]);
